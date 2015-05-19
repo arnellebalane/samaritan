c4a23cae3247a2d891ac47b86eed265efe662a89
Arnelle Balane <arnellebalane@gmail.com>
2015-05-20 03:05:57 +0800

    minimal interface with dummy timings

--- assets/javascripts/samaritan.js
+++ assets/javascripts/samaritan.js
@@ -2,8 +2,9 @@ var messaging = {
     $dom: $('.written-message .message'),
     _available: true,
     _t: null,
-    send: function(message) {
+    _send: function(message, callback) {
         if (messaging._available) {
+            messaging.clear();
             messaging._available = false;
             setTimeout(function() {
                 var characters = message.split('');
@@ -14,16 +15,68 @@ var messaging = {
                     } else {
                         messaging._available = true;
                         clearInterval(messaging._t);
+                        if (typeof callback === 'function') {
+                            callback();
+                        }
                     }
                 }, 50);
-            }, 1000);
+            }, 500);
         }
+    },
+    send: function(message, callback) {
+        messaging.$dom.parent().removeClass('important');
+        messaging._send(message, callback);
+    },
+    important: function(message, callback) {
+        setTimeout(function() {
+            messaging.$dom.parent().addClass('important');
+        }, 500);
+        messaging._send(message, callback);
     },
     clear: function() {
         if (messaging._available) {
             messaging.$dom.text('');
         }
+    },
+    blur: function() {
+        messaging.$dom.next('span').addClass('invisible');
+    },
+    focus: function() {
+        messaging.$dom.next('span').removeClass('invisible');
     }
 };
 
-messaging.send('System Initializing');
+
+
+$('textarea').on('keydown keyup', function(e) {
+    $(this).height(0);
+    $(this).height(this.scrollHeight);
+});
+
+$('button').on('click', function() {
+    var input = $('textarea').val().trim().replace(/\r?\n/g, ' ');
+    $('textarea, button').addClass('hidden');
+
+    messaging.focus();
+    messaging.send('Identifying Author', function() {
+        setTimeout(function() {
+            messaging.important('Author Identified', function() {
+                messaging.blur();
+            });
+        }, 2000);
+    });
+});
+
+
+
+messaging.send('Initializing System', function() {
+    setTimeout(function() {
+        messaging.important('Enter Input Text', function() {
+            messaging.blur();
+            $('textarea, button').removeClass('hidden');
+            setTimeout(function() {
+                $('textarea').focus();
+            }, 0);
+        });
+    }, 3000);
+});
