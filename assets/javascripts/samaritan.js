5697b317ffd59ec1e8234bc8511a2eb1ca2f4546
Arnelle Balane <arnellebalane@gmail.com>
2015-05-19 23:32:46 +0800

    back to simple interface

--- assets/javascripts/samaritan.js
+++
@@ -1,9 +0,0 @@
-var svg = {
-    element: $('#viewport'),
-    initialize: function() {
-        svg.element.attr('width', window.innerWidth);
-        svg.element.attr('height', window.innerHeight);
-    }
-};
-
-svg.initialize();
