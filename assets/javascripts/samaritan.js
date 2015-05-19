f3e601614f1ae1f874886165378aca7fe6738c5f
Arnelle Balane <arnellebalane@gmail.com>
2015-05-20 05:35:20 +0800

    exit animations

--- assets/javascripts/samaritan.js
+++ assets/javascripts/samaritan.js
@@ -19,8 +19,8 @@ var messaging = {
                             callback();
                         }
                     }
-                }, 50);
-            }, 500);
+                }, 40);
+            }, 250);
         }
     },
     send: function(message, callback) {
@@ -30,8 +30,12 @@ var messaging = {
     important: function(message, callback) {
         setTimeout(function() {
             messaging.$dom.parent().addClass('important');
-        }, 500);
+        }, 250);
         messaging._send(message, callback);
+    },
+    set: function(message) {
+        messaging.blur();
+        messaging.$dom.text(message);
     },
     clear: function() {
         if (messaging._available) {
@@ -39,30 +43,78 @@ var messaging = {
         }
     },
     blur: function() {
-        messaging.$dom.next('span').addClass('invisible');
+        messaging.$dom.next('span').addClass('hidden');
     },
     focus: function() {
-        messaging.$dom.next('span').removeClass('invisible');
+        messaging.$dom.next('span').removeClass('hidden');
     }
 };
 
 
 
-$('textarea').on('keydown keyup', function(e) {
+$('textarea').on('keydown keyup', function() {
     $(this).height(0);
     $(this).height(this.scrollHeight);
 });
 
-$('button').on('click', function() {
+$('.identify-author').on('click', function() {
     var input = $('textarea').val().trim().replace(/\r?\n/g, ' ');
-    $('textarea, button').addClass('hidden');
+    $('textarea').val('');
+    $('textarea, .identify-author').addClass('hidden');
 
     messaging.focus();
     messaging.send('Identifying Author', function() {
         setTimeout(function() {
-            messaging.send('Author Identified');
+            messaging.send('Author Identified', function() {
+                $('.again, .quit').removeClass('hidden');
+
+                var data = {
+                    name: 'Arnelle Balane',
+                    url: 'http://static.guim.co.uk/sys-images/Guardian/About/General/2011/11/27/1322410813055/Charles-Dickens-007.jpg',
+                };
+                $('.profile h1').text(data.name);
+                $('.profile .image').css('background-image',
+                    'url("' + data.url + '")');
+                $('.profile').removeClass('scaled');
+            });
         }, 2000);
     });
+});
+
+$('.again').on('click', function() {
+    $('.again, .quit').addClass('hidden');
+    $('.profile').addClass('scaled');
+
+    messaging.important('Enter Input Text', function() {
+        messaging.blur();
+        $('textarea, .identify-author').removeClass('hidden');
+        setTimeout(function() {
+            $('textarea').focus();
+        }, 0);
+    });
+});
+
+$('.quit').on('click', function() {
+    $('.again, .quit').addClass('hidden');
+    $('.profile').each(function() {
+        setTimeout(function() {
+            $(this).addClass('scaled');
+        }.bind(this), Math.random() * 3 * 1000);
+    });
+
+    messaging.$dom.parent().addClass('going-offline');
+    messaging.set('Terminating in :03');
+    messaging.blur();
+    var i = 2;
+    var t = setInterval(function() {
+        if (i--) {
+            messaging.set('Terminating in :0' + (i + 1));
+        } else {
+            clearInterval(t);
+            $('#viewport').addClass('offline');
+            messaging.set('Samaritan: Offline');
+        }
+    }, 1000);
 });
 
 
@@ -71,7 +123,7 @@ messaging.send('Initializing System', function() {
     setTimeout(function() {
         messaging.important('Enter Input Text', function() {
             messaging.blur();
-            $('textarea, button').removeClass('hidden');
+            $('textarea, .identify-author').removeClass('hidden');
             setTimeout(function() {
                 $('textarea').focus();
             }, 0);
@@ -89,3 +141,42 @@ profile.css({
     top: modal.offset().top + modal.outerHeight() + 30 + 'px',
     left: modal.offset().left - profile.outerWidth() / 2 + 'px'
 });
+
+
+
+// RANDOMIZING THE RANDOM THINGS
+setInterval(function() {
+    $('.random').each(function() {
+        var random = '';
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += '.';
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += '/';
+        random += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(~~(Math.random() * 26));
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += '-';
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += '.';
+        random += ~~(Math.random() * 9);
+        random += '.';
+        random += ~~(Math.random() * 9);
+        random += '.';
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += '-';
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        random += ~~(Math.random() * 9);
+        setTimeout(function() {
+            $(this).text(random);
+        }.bind(this), Math.random() * 3 * 100);
+    });
+}, 500);
